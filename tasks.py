c9981cc28d19257f1b635fa294063497b2455d45
Arthur Skowronek <eisensheng@mailbox.org>
2014-12-05 00:26:33 +0100

    Require setuptools 7.0 for tests.

--- tasks.py
+++ tasks.py
@@ -12,10 +12,11 @@ def mktravisconfig():
         env:
         {environs}
         install:
-            - pip install tox
+            - pip install tox setuptools==7.0
         script:
             - tox -e $TOX_ENV
     """)
     environs = ('\n'.join(('    - TOX_ENV=' + env)
-                          for env in parseconfig(None, 'tox').envlist))
+                          for env in parseconfig(None, 'tox').envlist
+                          if not env.startswith('cov-')))
     print(t.format(environs=environs))
