fe201399191979d50b42e4b973079247bcfee7e7
Arthur Skowronek <eisensheng@mailbox.org>
2014-12-06 00:16:24 +0100

    Create coverage report.

--- tasks.py
+++ tasks.py
@@ -5,18 +5,33 @@ from tox._config import parseconfig
 
 
 @task
-def mktravisconfig():
+def mk_travis_config():
+    """Generate configuration for travis."""
     t = dedent("""\
         language: python
         python: 3.4
         env:
-        {environs}
+        {jobs}
         install:
-            - pip install tox setuptools==7.0
+            - pip install -r requirements/ci.txt
         script:
-            - tox -e $TOX_ENV
+            - invoke ci_run_job $TOX_JOB
+        after_success:
+            coveralls
     """)
-    environs = ('\n'.join(('    - TOX_ENV=' + env)
-                          for env in parseconfig(None, 'tox').envlist
-                          if not env.startswith('cov-')))
-    print(t.format(environs=environs))
+    jobs = [env for env in parseconfig(None, 'tox').envlist
+            if not env.startswith('cov-')]
+    jobs += 'coverage',
+    print(t.format(jobs=('\n'.join(('    - TOX_JOB=' + job)
+                                   for job in jobs))))
+
+
+@task
+def ci_run_job(job_name):
+    """Run given job name in tox environment.  This task is supposed to run
+    within the CI environment.
+    """
+    run(('tox'
+         + ((' -e ' + job_name) if job_name != 'coverage' else '')
+         + ' -- --color=yes'),
+        pty=True)
